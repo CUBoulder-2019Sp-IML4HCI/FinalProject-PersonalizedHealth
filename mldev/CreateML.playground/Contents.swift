e72756c7482a0cffab79d0370c6420a7784b0ea7
Akshit Arora <akshit.arora@colorado.edu>
2019-04-26 13:13:14 -0600

    corrected stage calculator

--- mldev/CreateML.playground/Contents.swift
+++ mldev/CreateML.playground/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 
 let data = try MLDataTable(contentsOf: URL(fileURLWithPath: "/Users/akshitarora/Documents/GitHub/courses/ml_hci/FinalProject-PersonalizedHealth/mldev/simulated.json"))
 
-let regressor = try MLDecisionTreeClassifier(trainingData: data, targetColumn: "stage", featureColumns: ["jog_abs_time", "jog_frac_time", "jog_abs_dist", "jog_frac_dist"])
+let regressor = try MLDecisionTreeClassifier(trainingData: data, targetColumn: "stage", featureColumns: ["tot_time", "tot_dist", "pace"])
 
 let metadata = MLModelMetadata(author: "Akshit Arora", shortDescription: "A model trained to predict stage in a couch to 5k plan.", version: "1.0")
 
